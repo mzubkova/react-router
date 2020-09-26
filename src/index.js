38afa7078c114b809e338c5dacdc66ddcf93e5f3
mzubkova <59561550+mzubkova@users.noreply.github.com>
2020-09-26 19:06:41 +0300

    redirect

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "fontsource-roboto";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>,
+  </HashRouter>,
   document.getElementById("root")
 );
