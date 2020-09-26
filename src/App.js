15979a29f7c7cd74befa132765d57a8ff365564b
mzubkova <59561550+mzubkova@users.noreply.github.com>
2020-09-26 17:23:52 +0300

    added redirect

--- src/App.js
+++ src/App.js
@@ -46,7 +46,12 @@ class App extends Component {
           <Header />
           <Switch>
             {MENU.map(({ path, component, exact = false }) => (
-              <Route path={path} exact={exact} component={component} />
+              <Route
+                path={path}
+                exact={exact}
+                component={component}
+                render={() => <Redirect to={path} />}
+              />
             ))}
             <Route path="/" exact render={() => <Redirect to="/main" />} />
           </Switch>
